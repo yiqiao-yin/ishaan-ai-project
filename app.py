2781221442a5c7cfe88fbc9db9d00508cc3e6cf5
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2023-07-21 18:28:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 import streamlit as st
 
 st.write("hello world Yiqiao!")
+
+def call_chatgpt(prompt: str) -> str:
+    """
+    Uses the OpenAI API to generate an AI response to a prompt.
+
+    Args:
+        prompt: A string representing the prompt to send to the OpenAI API.
+
+    Returns:
+        A string representing the AI's generated response.
+
+    """
+
+    # Use the OpenAI API to generate a response based on the input prompt.
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.3,
+        max_tokens=800,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    # Extract the text from the first (and only) choice in the response output.
+    ans = response.choices[0]["text"]
+
+    # Return the generated AI response.
+    return ans
